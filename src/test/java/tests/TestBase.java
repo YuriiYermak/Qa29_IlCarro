1e3252a1d18ec73ed740593104ef7ec400450ef4
YuriiYermak <35467448+YuriiYermak@users.noreply.github.com>
2021-09-15 15:55:25 +0300

    added login test

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,73 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.navigate().to("https://ilcarro.xyz/search");
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        wd.quit();
+    }
+
+    //*******************************************************
+
+    private void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        if (text != null) {
+            WebElement element = wd.findElement(locator);
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public void openLoginForm() {
+        click(By.xpath("//a[text()='Log in']"));
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.id("email"), email);
+        type(By.id("password"), password);
+    }
+
+    public void submitForm() {
+        click(By.xpath("//button[@type='submit']"));
+    }
+
+    public boolean isLogged() {
+        String text = wd.findElement(By.cssSelector(".dialog-container h2")).getText();
+        click(By.xpath("//button[text()='Ok']"));
+        return text.equals("Logged in success");
+    }
+
+    public void logout() {
+        click(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public boolean isLogOutPresent() {
+        return isElementPresent(By.xpath("\"//a[text()=' Logout ']\""));
+    }
+
+    public boolean isElementPresent(By locator) {
+        return wd.findElements(locator).size()>0;
+    }
+}
